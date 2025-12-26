e9897a2e1522e1870210e109ec7fcd97f0b32985
IamShivanshCoder <shivanshbansal0308@gmail.com>
2025-12-26 21:15:31 +0530

    commit

---
+++ 02.Advanced/04_objects.js
@@ -0,0 +1,42 @@
+//const tinderUser = new Object()
+
+const tinderUser = {};
+tinderUser.id = "e";
+tinderUser.name = "sam";
+tinderUser.isloggedin = false;
+
+console.log(tinderUser);
+
+// const regUser = {
+//     email: "some@gmail.com",
+//     fulName: {
+//         userfullname: {
+//             firstname: "shivansh",
+//             lastname: "bansal",
+//         },
+//     },
+// };
+
+obj1 = { 1: "A", 2: "B", 3: "c" };
+obj2 = { 4: "f" };
+
+const obj3 = { ...obj1, ...obj2 }; //Preferred method
+
+
+
+const users = [
+    {
+        id:1,
+        email:"s@gmail.cpm"
+    },
+    {
+        id:2,
+        email:"a@gmail.com"
+    }
+]
+
+console.log(
+    Object.keys(...users)
+);
+
+console.log(users[0].hasOwnProperty('isLogg')) //find prop
